403f74991feb2b2c6af3c8d184fc52b35869faad
zenjause <jesus.nf22@gmail.com>
2023-01-13 11:15:59 -0500

    desafio

---
+++ desafio.js
@@ -0,0 +1,12 @@
+const nombreApellido = "Jesus Narciso";
+const edad = 30;
+const desarrollador = true;
+let fechaHappyBirthday = new Date ("1992 june 22");
+let fecha = new Date("1982 ")
+const libro={
+    titulo: " gerente en un minuto",
+    autor:"Ken Blanchard",
+    fecha:fecha,
+    
+}
+let url = new URL("https://html.rincondelvago.com/el-gerente-al-minuto_ken-blanchard.html");
