275f14998c87fac9618dc1dca9ef7b6741f7fad2
zenjause <jesus.nf22@gmail.com>
2023-01-22 12:08:34 -0500

    desafio completado

---
+++ desafio-cadena-de-texto.js
@@ -0,0 +1,10 @@
+let Nombre = " Jesus";
+let Apellido = "Narciso ";
+let estudiante = Nombre +" "+ Apellido;
+let estudianteMayuscula = estudiante.toUpperCase(estudiante);
+let estudianteMinuscula = estudiante.toLowerCase(estudiante);
+let contadorLetras = estudiante.length;
+let primeraLetra = Nombre.charAt(1);
+let ultimaLetra = Apellido.charAt(6)
+let sinEspacios = estudiante.trim().length;
+let boolen_nombre= Nombre.startsWith("Jesus");
